31945be9e80ad62032b7fcc965a5e8c307705d04
Amr Khaled Marey <112765490+AmrKhaled03@users.noreply.github.com>
2023-05-27 21:01:28 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+var btn=document.querySelector(".btn_open").addEventListener("click",openNav);
+var side=document.getElementById("aside");
+var spn=document.getElementsByTagName("span");
+function openNav(){
+side.classList.toggle("active");
+
+
+}
